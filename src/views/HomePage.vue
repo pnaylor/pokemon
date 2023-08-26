ac59f75489d9474dac27d752d5dd0750983b21e7
Paul N <pauln@ori.org>
2023-08-25 21:02:39 -0700

    layout basics

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,55 +2,115 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>All The Pokemon!</ion-title>
       </ion-toolbar>
     </ion-header>
 
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+      <ion-grid>
+        <ion-row v-for="pokemonRow in pokemonVisible">
+          <ion-col v-for="pokemon in pokemonRow">
+            <ion-card>
+              <ion-avatar>
+                <img :src="getSpriteLink(pokemon.url)" />
+              </ion-avatar>
+              <ion-card-content class="ion-text-capitalize">
+                {{ pokemon.name }}
+              </ion-card-content>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/vue";
+import { computed, watchEffect, ref } from "vue";
+import { useFetch } from "@vueuse/core";
+
+interface Pokemon {
+  name: string;
+  url: string;
+}
+
+interface PokeData {
+  results: Pokemon[];
+}
+
+const { isFetching, error, data } = useFetch<PokeData>(
+  "https://pokeapi.co/api/v2/pokemon?limit=24",
+  {
+    afterFetch(response) {
+      response.data = JSON.parse(response.data);
+      return response;
+    },
+  }
+);
+
+const pokemonById = ref<Pokemon[]>([]); // sorted by ID
+const pokemonByName = ref<Pokemon[]>([]); // sorted by name
+const pokemonVisible = ref<Pokemon[][]>([]); // showing 12 at a time
+
+function convertTo2DArray(array: Pokemon[], columns: number) {
+  const result = [];
+  for (let i = 0; i < array.length; i += columns) {
+    result.push(array.slice(i, i + columns));
+  }
+  return result;
+}
+
+watchEffect(() => {
+  if (!isFetching.value) {
+    pokemonById.value = data.value?.results || [];
+    let pokemonSlice = pokemonById.value?.slice(0, 12);
+    pokemonVisible.value = convertTo2DArray(pokemonSlice, 4);
+
+    pokemonByName.value = pokemonById.value.sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    });
+  }
+});
+
+const spriteUrl: string =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
+function getSpriteLink(url?: string) {
+  let segments = url ? url.split("/") : [];
+  segments = segments.filter((segment) => segment !== "");
+  let id = parseInt(segments.pop() || "1");
+  return `${spriteUrl}${id}.png`;
+}
+
+function updatePokemonVisible() {}
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+ion-avatar {
+  float: left;
+  margin: 12px 0 12px 12px;
+  background-color: gainsboro;
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
+ion-card {
+  align-items: center;
+  display: flex;
 }
 
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
+ion-card-content {
+  color: black;
 }
 
-#container a {
-  text-decoration: none;
+@media (prefers-color-scheme: dark) {
+  ion-card-content {
+    color: white;
+  }
 }
 </style>
